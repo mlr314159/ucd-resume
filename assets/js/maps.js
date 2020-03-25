9e0b2981b1a12800fd613e0d31076caf0ec274ce
Mike <mlr314159@yahoo.co.uk>
2020-03-25 14:16:45 +0000

    Google Maps  - Code tidied up

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+//var APIKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+function initMal() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 40.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new markerCluster(map, markers, { imagePath: 'xx https://developers.google.com/maps/documentation/examples/markcluster/m' });
+}
